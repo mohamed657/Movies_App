d82c270b5894dee978a7e36b7f9095da90e4a294
mohamed657 <Mohamedhany123@>
2021-11-28 21:49:22 +0200

    Edit SearchTap size & border textfilled

--- lib/Ui/Search/Search_Tap.dart
+++ lib/Ui/Search/Search_Tap.dart
@@ -5,50 +5,45 @@ class Search_Tap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.all(15),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
-            height: 30,
+            height: MediaQuery.of(context).size.height * 0.03,
           ),
           Container(
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border(),
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border(
+                    top: BorderSide(color: Colors.grey, width: 1),
+                    bottom: BorderSide(color: Colors.grey, width: 1),
+                    left: BorderSide(color: Colors.grey, width: 1),
+                    right: BorderSide(color: Colors.grey, width: 1),
+                  ),
                   color: MyThemeData.bottomNvigationcolotr),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border(
-                      top: BorderSide(color: Colors.grey, width: 1),
-                      bottom: BorderSide(color: Colors.grey, width: 1),
-                      left: BorderSide(color: Colors.grey, width: 1),
-                      right: BorderSide(color: Colors.grey, width: 1),
-                    )),
-                child: TextField(
-                  decoration: InputDecoration(
-                      disabledBorder: UnderlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: Colors.transparent)),
-                      icon: IconButton(
-                        splashColor: Colors.transparent,
-                        onPressed: () {},
-                        icon: ImageIcon(
-                          AssetImage('assets/images/search_icon.png'),
-                          color: Colors.white,
-                        ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: IconButton(
+                      splashColor: Colors.transparent,
+                      onPressed: () {},
+                      icon: ImageIcon(
+                        AssetImage('assets/images/search_icon.png'),
+                        color: Colors.white,
                       ),
-                      hintStyle: TextStyle(
-                        color: Color.fromARGB(255, 112, 112, 112),
-                      ),
-                      hintText: 'Search'),
-                ),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Color.fromARGB(255, 112, 112, 112),
+                    ),
+                    hintText: 'Search'),
               )),
-          Image.asset(
-            'assets/images/notfoundicon.png',
-            height: MediaQuery.of(context).size.height * 0.7,
+          Expanded(
+            child: Image.asset(
+              'assets/images/notfoundicon.png',
+              height: MediaQuery.of(context).size.height * 0.7,
+            ),
           )
         ],
       ),
